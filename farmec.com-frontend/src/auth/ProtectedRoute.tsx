10a9daa3d40ac302333740c32dadbf051b586cf2
kimaiyo <meshackkimaiyo5@gmail.com>
2025-04-23 21:56:41 +0300

    fix: update caniuse-lite version and add license in package-lock.json; refactor ProtectedRoute component for clarity; update tailwind.config.js to use ES module syntax

--- farmec.com-frontend/src/auth/ProtectedRoute.tsx
+++ farmec.com-frontend/src/auth/ProtectedRoute.tsx
@@ -1,16 +1,15 @@
-// import { useAuth0 } from "@auth0/auth0-react";
-// import { Navigate, Outlet } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Navigate, Outlet } from "react-router-dom";
 
-// const ProtectedRoute = () => {
-//   const { isAuthenticated,isLoading } = useAuth0();
-//   if(isLoading){
-//     return null
-//   }
-//   if(isAuthenticated){
-//     return <Outlet/>
-//   }
-//   return <Navigate to="/" replace/>
-  
-// }
+const ProtectedRoute = () => {
+  const { isAuthenticated, isLoading } = useAuth0();
+  if (isLoading) {
+    return null;
+  }
+  if (isAuthenticated) {
+    return <Outlet/>;
+  }
+  return <Navigate to="/" replace/>;
+};
 
-// export default ProtectedRoute;
+export default ProtectedRoute;
